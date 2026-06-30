fab91d6efe74d3e5d279ad96f5181c547ad7d676
jon caulfield <jon.caulfield@example.com>
2026-06-30 16:19:20 -0500

    feat: process received message via use-case-handler

--- application/src/main/java/zen/lab/consumer/adapters/edge/inbound/messages/ClickEventMessageConsumer.java
+++ application/src/main/java/zen/lab/consumer/adapters/edge/inbound/messages/ClickEventMessageConsumer.java
@@ -1,17 +1,20 @@
 package zen.lab.consumer.adapters.edge.inbound.messages;
 
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import zen.lab.consumer.adapters.edge.inbound.messages.topology.Schema;
+import zen.lab.consumer.application.port.in.MessageConsumerUseCase;
 import zen.lab.consumer.domain.events.ClickEvent;
 
 @Component
-@Slf4j
+@RequiredArgsConstructor
 public class ClickEventMessageConsumer {
+
+    private final MessageConsumerUseCase messageConsumerUseCase;
 
     @KafkaListener(topics = Schema.Topics.CLICK_EVENT_RECEIVED)
     public void consume(ClickEvent event) {
-        log.info(">>> Received event: {}", event);
+        messageConsumerUseCase.process(event);
     }
 }
