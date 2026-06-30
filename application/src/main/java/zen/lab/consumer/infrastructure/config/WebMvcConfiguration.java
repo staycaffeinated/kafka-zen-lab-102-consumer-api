ede558d46a14e81c4573f6dff7a1e5e5fa2e0a63
jon caulfield <jon.caulfield@example.com>
2026-06-30 16:17:38 -0500

    feat: apply cors filtering

--- application/src/main/java/zen/lab/consumer/infrastructure/config/WebMvcConfiguration.java
+++ application/src/main/java/zen/lab/consumer/infrastructure/config/WebMvcConfiguration.java
@@ -3,27 +3,16 @@
  */
 package zen.lab.consumer.infrastructure.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@SuppressWarnings({"java:S6857" // false positive; property names may contain dashes
-})
+@RequiredArgsConstructor
 public class WebMvcConfiguration implements WebMvcConfigurer {
 
-    /**
-     * This allows you to set the allowed-origins in an environment variable or
-     * application.yaml/properties. Its not unusual to have a variety of origins in
-     * a cloud environment; for example, something like: api.acme.com,
-     * admin.acme.com, some-application.acme.com.
-     * The default, localhost, is usually suitable for development.
-     *
-     * There are no other dependencies on this property name; rename it if you want.
-     */
-    @Value("${application.cors.allowed-origins:http://*.localhost}")
-    private String allowedOriginsPattern;
+    private final ApplicationProperties applicationProperties;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -31,6 +20,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
-                .allowedOriginPatterns(allowedOriginsPattern);
+                .allowedOriginPatterns(applicationProperties.cors().allowedOrigins());
     }
 }
