964a094a2884eb91a1650d111a39de101972abd3
jon caulfield <jon.caulfield@example.com>
2026-07-26 19:12:44 -0500

    refactor: add @ActiveProfiles({TEST, INTEGRATION_TEST}) and @TestPropertySource(properties = spring.kafka.listener.auto-startup=false)

--- application/src/integrationTest/java/zen/lab/consumer/ApplicationIntegrationTest.java
+++ application/src/integrationTest/java/zen/lab/consumer/ApplicationIntegrationTest.java
@@ -4,13 +4,19 @@
 package zen.lab.consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static zen.lab.consumer.adapters.shared.profiles.SpringProfiles.INTEGRATION_TEST;
+import static zen.lab.consumer.adapters.shared.profiles.SpringProfiles.TEST;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles({TEST, INTEGRATION_TEST})
+@TestPropertySource(properties = "spring.kafka.listener.auto-startup=false")
 class ApplicationIntegrationTest {
 
     @Autowired
